d3c877fddecb702a9dfeb24fb7c600eec22bab17
infinitelytight <6633057+infinitelytight@users.noreply.github.com>
2019-09-30 15:34:55 +0100

    Don't count NC as draw

--- mma.r
+++ mma.r
@@ -1,10 +1,10 @@
 options(error=traceback)
 
 get_score <- function(fighter, scores) {
-  scores[scores[, "Fighter"] == fighter, ]$Score
+  scores[fighter, ]$Score
 }
 
-update_scores <- function(fight, scores) {
+update_scores <- function(fight, scores, k=32) {
   fighter <- as.character(fight$Fighter)
   opponent <- as.character(fight$Opponent)
   result <- as.character(fight$Result)
@@ -29,22 +29,32 @@ update_scores <- function(fight, scores) {
   #  f_result = 0
   #  o_result = 1
   #}
-  # This counts no contests as draws
   else if (result == "vs." || result == "vs") {
-    f_result = 0.5
-    o_result = 0.5
+    if (result == "No Contest" || result == "No contest") {
+      return(scores)
+    } else {
+      f_result = 0.5
+      o_result = 0.5
+    }
   }
   else {
     return(scores)
   }
 
-  f_new_score = round(f_score + 32 * (f_result - f_exp))
-  o_new_score = round(o_score + 32 * (o_result - o_exp))
+  f_new_score = round(f_score + k * (f_result - f_exp))
+  o_new_score = round(o_score + k * (o_result - o_exp))
 
   scores[scores[, "Fighter"] == fighter, ]$Score <- f_new_score
   scores[scores[, "Fighter"] == opponent, ]$Score <- o_new_score
 
   return(scores)
+}
+
+save_rankings <- function(rankings, filename) {
+  sink(filename)
+  print(data.frame(Rank=1:nrow(rankings), rankings), row.names=FALSE)
+  closeAllConnections()
+  print(paste(c("Full list saved in", filename), collapse=" "))
 }
 
 parse_date <- function(date_string) {
@@ -71,11 +81,17 @@ fighters <- unique(all_names)
 
 scores <- data.frame(fighters, 1000)
 names(scores) <- c("Fighter", "Score")
+rownames(scores) <- scores$Fighter
 
 for (i in 1:nrow(results)) {
   scores <- update_scores(results[i, ], scores)
 }
 
 ranked <- scores[order(-scores$Score), ]
+
 top <- ranked[1:30, ]
-print(top)
+print("Top 30:")
+print(top, row.names=FALSE)
+
+# Uncomment to save full list to file
+# save_rankings(ranked, "rankings.txt")
